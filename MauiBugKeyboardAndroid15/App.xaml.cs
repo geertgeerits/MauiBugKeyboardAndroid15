f1dc28a89b4c46fbffadd4cf77b7f23a74552035
Geert Geerits <ggeerits@hotmail.com>
2024-11-17 09:41:03 +0100

    Add project files.

---
+++ MauiBugKeyboardAndroid15/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiBugKeyboardAndroid15
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
